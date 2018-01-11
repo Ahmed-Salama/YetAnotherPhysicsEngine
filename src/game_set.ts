7e7bf1c411e802143c2872961da5eae25ec7fa91
Ahmed Salama <ahmedsalama183@gmail.com>
2018-01-10 22:42:32 -0800

    Remove dead code

--- src/game_set.ts
+++ src/game_set.ts
@@ -28,7 +28,7 @@ export default class GameSet extends GameElement {
     this.contents = Immutable.Map([
         [my_car.id, my_car],
         [ground.id, ground],
-        // [ball.id, ball],
+        [ball.id, ball],
     ]);
 
     this.camera = new Camera(true);
@@ -97,72 +97,29 @@ export default class GameSet extends GameElement {
 
     const updated_self = final_delta.game_set.contents.get(object.id);
     const multiplier = final_delta.collided_objects.size > 0 ? 
-      final_delta.game_set.binary_search_no_collision(updated_self, final_delta.position, final_delta.angle) :
+      final_delta.game_set._is_non_intersecting_delta_for_object(updated_self, final_delta.position, final_delta.angle) :
       1;
 
     const final_delta_p = final_delta.position.multiply(multiplier);
     const final_delta_a = final_delta.angle * multiplier;
 
-    if (multiplier < 1) console.log(multiplier);
     const updated_self_with_delta = updated_self.move(final_delta_p).rotate(final_delta_a);
     const final_game_set = final_delta.game_set.replace_element(updated_self_with_delta);
 
     return final_game_set;
-
-    // if (multiplier < limit) {
-    //   // Apply contact velocities
-    //   return final_delta.collided_objects.reduce(
-    //     (current_game_set, other) => {
-    //       const updated_self = current_game_set.contents.get(object.id) as PhysicalObject;
-    //       const updated_other = current_game_set.contents.get(other) as PhysicalObject;
-
-    //       return current_game_set.add_contact_velocities(updated_self, updated_other);
-    //     },
-    //     final_delta.game_set);
-    // } else {
-    //   return final_game_set;
-    // }
   }
 
-  // private add_contact_velocities(o_a: PhysicalObject, o_b: PhysicalObject): GameSet {
-  //   const factor = 1;
-  //   const v_ab = o_b.is_ground ? o_b.o_a.position.to(o_b.position).normalize();
+  private _is_non_intersecting_delta_for_object(object: PhysicalObject, delta_p: Vector2D, delta_a: number) {
+    const object_with_delta_p = object.move(delta_p);
+    const object_with_delta_a = object.rotate(delta_a);
 
-  //   const next_o_a = o_a.copy<PhysicalObject>({ velocity: o_a.velocity.add_vector(v_ab.reverse().multiply(factor))});
-  //   const next_o_b = o_b.copy<PhysicalObject>({ velocity: o_b.velocity.add_vector(v_ab.multiply(factor))});
+    const intersects = this
+      .filter_contents(o => o.id != object.id)
+      .some(other => 
+        object_with_delta_p.calculate_collision(other).collided() ||
+        object_with_delta_a.calculate_collision(other).collided());
 
-  //   return this.replace_element(next_o_a).replace_element(next_o_b);
-  // }
-
-  private binary_search_no_collision(object: PhysicalObject, delta_p: Vector2D, delta_a: number) {
-    const can = (middle: number) => {
-      const delta_p_middle = delta_p.multiply(middle);
-      const delta_a_middle = delta_a * middle;
-
-      const object_with_delta_p = object.move(delta_p_middle);
-      const object_with_delta_a = object.rotate(delta_a_middle);
-
-      const intersects = this
-        .filter_contents(o => o.id != object.id)
-        .some(other => 
-          object_with_delta_p.calculate_collision(other).collided() ||
-          object_with_delta_a.calculate_collision(other).collided());
-
-      return !intersects;
-    }
-    const binary_search_no_collision_rec = (low: number, high: number, iterations: number): number => {
-      if (iterations == 0) {
-        if (can(high)) return high;
-        else return low;
-      }
-
-      const middle = (low + high) * 1.0 / 2;
-
-      if (!can(middle)) return binary_search_no_collision_rec(low, middle, iterations - 1);
-      else return binary_search_no_collision_rec(middle, high, iterations - 1);
-    }
-
-    return binary_search_no_collision_rec(0, 1, 0);
+    return intersects ? 0 : 1;
   }
 
   public _collide(o_a: PhysicalObject, o_b: PhysicalObject, delta_position: Vector2D, delta_angle: number, game_set: GameSet): CollisionResult {
@@ -215,18 +172,16 @@ export default class GameSet extends GameElement {
         const impulse = - impulse_weight * (1 + elasticity) * v_ap1.dot(normal) /
             (1.0/m_a + Math.pow(r_ap.cross(normal), 2)/i_a);
 
-        const new_delta_p = delta_p.rotate(-normal.angle()).mapX(x => Math.max(0, x)).rotate(normal.angle());
+        const new_delta_p = delta_p.rotate(-normal.angle()).mapX(x => x > 0 ? x : -x).rotate(normal.angle());
 
-        const d_v_a = normal.multiply(impulse / m_a)//.add_vector(normal.multiply(delta_p.length() - new_delta_p.length() + contact_minimum));
+        const d_v_a = normal.multiply(impulse / m_a)
         const d_w_a = r_ap.cross(normal.multiply(impulse)) / i_a;
-
 
         return collide_rec(delta_v_a.add_vector(d_v_a), delta_w_a + d_w_a, new_delta_p,
                            remaining_intersections.shift());
       }
 
       const delta = collide_rec(Vector2D.empty, 0, delta_position, collision.intersections);
-      console.log("reflect: " + delta.toString());
 
       return {
         game_set: game_set.replace_element(o_a.copy({
@@ -236,8 +191,6 @@ export default class GameSet extends GameElement {
         collision: collision,
         delta_position: delta.d_p,
         delta_angle: 0
-        // delta_angle: (delta_angle > 0 ? 1 : -1) * 0.01
-        // delta_angle: delta_angle * delta.d_p.length() / delta_position.length()
       };
     } else {
       const collide_rec = (delta_v_a: Vector2D, delta_w_a: number, delta_v_b: Vector2D,
@@ -287,16 +240,10 @@ export default class GameSet extends GameElement {
             (1.0/m_a + 1.0/m_b + Math.pow(r_ap.cross(normal), 2)/i_a +
             Math.pow(r_bp.cross(normal), 2)/i_b);
 
-        // alert("Self: " + advanced_self.position +
-            // "\no_b: " + o_b.position + 
-            // "\nIntersection: " + intersection_point + 
-            // "\nImpulse: " + impulse +
-            // "\nVelocity: " + v_ab1.dot(normal) +
-            // "\nTerm: " + (1.0/m_a + 1.0/m_b));
-
         const normal_reverse = normal.reverse();
         const new_delta_p = delta_p.rotate(-normal_reverse.angle()).mapX(x => Math.max(0, x)).rotate(normal_reverse.angle());
 
+        // New velocities calculated based on impulse and contact forces
         const d_v_a = normal.multiply(impulse / m_a).add_vector(normal_reverse.multiply(delta_p.length() - new_delta_p.length() + contact_minimum * m_b / (m_a + m_b)));
         const d_v_b = normal.multiply(-impulse / m_b).add_vector(normal.multiply(delta_p.length() - new_delta_p.length() + contact_minimum * m_a / (m_a + m_b)));
 
@@ -325,7 +272,6 @@ export default class GameSet extends GameElement {
         game_set: game_set.replace_element(updated_self).replace_element(updated_other),
         collision: collision,
         delta_position: delta.d_p,
-        // delta_angle: (delta_angle > 0 ? 1 : -1) * 0.01
         delta_angle: 0
       };
     }

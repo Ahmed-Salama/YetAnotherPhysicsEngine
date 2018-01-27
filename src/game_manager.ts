5d16cef486d800f6c5508abfd6d0073ecac11a69
Ahmed Salama <ahmedsalama183@gmail.com>
2018-01-26 23:40:25 -0800

    Add elapsed time score!

--- src/game_manager.ts
+++ src/game_manager.ts
@@ -6,15 +6,18 @@ import GameLevel3 from "./game_level_3";
 import GameLevel4 from "./game_level_4";
 import GameLevel5 from "./game_level_5";
 import GameLevel2s0 from "./game_level_2_0";
+import Constants from "./constants";
 
 
 export default class GameManager extends GameElement {
   public game_level_managers: Immutable.Map<number, GameLevelManager>;
   public current_game_level_manager_id: number;
   public finished: boolean;
+  public time_elapsed: number;
 
   constructor(initialize: boolean) {
     super(initialize);
+    this.time_elapsed = 0;
   }
 
   protected initialize() {
@@ -32,22 +35,25 @@ export default class GameManager extends GameElement {
   public updated(time_unit: number): GameManager {
     if (this.finished) return this;
 
+    const new_time_elapsed = this.time_elapsed + time_unit;
+
     const current_game_level_manager = this.game_level_managers.get(this.current_game_level_manager_id);
     const updated_current_game_level_manager = current_game_level_manager.updated(time_unit);
 
     if (updated_current_game_level_manager.finished) {
       if (this.current_game_level_manager_id + 1 >= this.game_level_managers.size) {
-        return this.copy({ finished: true });
+        return this.copy({ finished: true, time_elapsed: new_time_elapsed });
       } else {
-        return this.copy({ current_game_level_manager_id: this.current_game_level_manager_id + 1 });
+        return this.copy({ current_game_level_manager_id: this.current_game_level_manager_id + 1, time_elapsed: new_time_elapsed });
       }
     } else {
-      return this.copy({ game_level_managers: this.game_level_managers.set(this.current_game_level_manager_id, updated_current_game_level_manager)});
+      return this.copy({ game_level_managers: this.game_level_managers.set(this.current_game_level_manager_id, updated_current_game_level_manager), time_elapsed: new_time_elapsed });
     }
   }
 
   public draw(ctx: CanvasRenderingContext2D) {
     ctx.save();
+    this.game_level_managers.get(this.current_game_level_manager_id).draw(ctx);
     ctx.fillStyle = "white";
     ctx.font = "14px Arial";
     ctx.fillText("[ UP / DOWN ] Ground movement", 10, 20);    
@@ -55,8 +61,9 @@ export default class GameManager extends GameElement {
     ctx.fillText("[ D ] Jump / Dodge", 10, 60);    
     ctx.fillText("[ F ] Nitro", 10, 80);    
     ctx.fillText("[ V ] Flip backwards", 10, 100);    
-    ctx.fillText("[ C ] Flip", 10, 120);    
-    this.game_level_managers.get(this.current_game_level_manager_id).draw(ctx);
+    ctx.fillText("[ C ] Flip", 10, 120);
+    ctx.font = "bold 20px Arial";
+    ctx.fillText("Time elapsed: " + (this.time_elapsed / 1000 / Constants.time_scale).toFixed(2), 10, 160);
     ctx.restore();
   }
 }

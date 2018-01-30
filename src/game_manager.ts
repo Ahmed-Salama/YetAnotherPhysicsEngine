defa069beabf35d6254374ae38f1f9e1c2912580
Ahmed Salama <ahmedsalama183@gmail.com>
2018-01-29 20:25:11 -0800

    Add game level indicator

--- src/game_manager.ts
+++ src/game_manager.ts
@@ -12,6 +12,7 @@ import Constants from "./constants";
 export default class GameManager extends GameElement {
   public game_level_managers: Immutable.Map<number, GameLevelManager>;
   public current_game_level_manager_id: number;
+  public max_game_level_id: number;
   public finished: boolean;
   public time_elapsed: number;
 
@@ -22,14 +23,15 @@ export default class GameManager extends GameElement {
 
   protected initialize() {
     this.game_level_managers = Immutable.Map<number, GameLevelManager>([
-      [0, new GameLevelManager(true, new GameLevel1(true))],
-      [1, new GameLevelManager(true, new GameLevel2s0(true))],
-      [2, new GameLevelManager(true, new GameLevel3(true))],
-      [3, new GameLevelManager(true, new GameLevel2(true))],
-      [4, new GameLevelManager(true, new GameLevel4(true))],
-      [5, new GameLevelManager(true, new GameLevel5(true))],
+      [1, new GameLevelManager(true, new GameLevel1(true))],
+      [2, new GameLevelManager(true, new GameLevel2s0(true))],
+      [3, new GameLevelManager(true, new GameLevel3(true))],
+      [4, new GameLevelManager(true, new GameLevel2(true))],
+      [5, new GameLevelManager(true, new GameLevel4(true))],
+      [6, new GameLevelManager(true, new GameLevel5(true))],
     ]);
     this.current_game_level_manager_id = this.game_level_managers.keySeq().min();
+    this.max_game_level_id = this.game_level_managers.keySeq().max();
   }
 
   public updated(time_unit: number): GameManager {
@@ -41,7 +43,7 @@ export default class GameManager extends GameElement {
     const updated_current_game_level_manager = current_game_level_manager.updated(time_unit);
 
     if (updated_current_game_level_manager.finished) {
-      if (this.current_game_level_manager_id + 1 >= this.game_level_managers.size) {
+      if (this.current_game_level_manager_id + 1 > this.max_game_level_id) {
         return this.copy({ finished: true, time_elapsed: new_time_elapsed });
       } else {
         return this.copy({ current_game_level_manager_id: this.current_game_level_manager_id + 1, time_elapsed: new_time_elapsed });
@@ -54,10 +56,16 @@ export default class GameManager extends GameElement {
   public draw(ctx: CanvasRenderingContext2D) {
     ctx.save();
     this.game_level_managers.get(this.current_game_level_manager_id).draw(ctx);
+
+    const draw_text = (text: string, x: number, y: number) => {
+      ctx.fillText(text, x, y);
+      ctx.strokeText(text, x, y);
+    }
+
     ctx.fillStyle = "white";
-    ctx.font = "bold 14px Arial";
+    ctx.font = "bold 14px verdana";
     ctx.fillText("CAR CONTROLS: ", 10, 20);    
-    ctx.font = "14px Arial";
+    ctx.font = "14px verdana";
     ctx.fillText("[ UP / DOWN ] Ground movement", 10, 40);    
     ctx.fillText("[ LEFT / RIGHT ] Rotate in the air", 10, 60);    
     ctx.fillText("[ D ] Jump / Dodge", 10, 80);    
@@ -65,13 +73,19 @@ export default class GameManager extends GameElement {
     ctx.fillText("[ V ] Flip backwards", 10, 120);    
     ctx.fillText("[ C ] Flip", 10, 140);
 
-    ctx.font = "bold 14px Arial";
+    ctx.font = "bold 14px verdana";
     ctx.fillText("LEVEL CONTROLS: ", 10, 170);    
-    ctx.font = "14px Arial";
+    ctx.font = "14px verdana";
     ctx.fillText("[ Q ] Reset level", 10, 190);
 
-    ctx.font = "bold 20px Arial";
+    ctx.font = "bold 20px verdana";
     ctx.fillText("Time elapsed: " + (this.time_elapsed / 1000 / Constants.time_scale).toFixed(2), 10, 230);
+
+    ctx.font = "bold 30px verdana";
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    draw_text("LEVEL", 1200/2 - 40, 30);
+    draw_text(this.current_game_level_manager_id + "", 1200/2, 60);
     ctx.restore();
   }
 }
